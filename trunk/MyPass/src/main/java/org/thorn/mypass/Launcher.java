19f61f9b79aa5164a886dc4fb327f8f545f6b8a5
chenyun.chris@gmail.com <chenyun.chris@gmail.com@e9b15cee-acf9-eff9-f6f9-57fa9600d596>
2013-06-07 05:33:16 +0000

    Failed commit: Default

--- trunk/MyPass/src/main/java/org/thorn/mypass/Launcher.java
+++ trunk/MyPass/src/main/java/org/thorn/mypass/Launcher.java
@@ -5,7 +5,7 @@ import java.awt.EventQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.thorn.core.context.SpringContext;
-import org.thorn.mypass.utils.ImageUtils;
+import org.thorn.core.swing.ImageUtils;
 import org.thorn.mypass.view.ComponentReference;
 import org.thorn.mypass.view.MainFrame;
 
